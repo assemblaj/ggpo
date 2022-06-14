095b6f25782e9cd6ab2fe23d5a3f3970498249c4
Assem Blaj <assemblaj@notreal.com>
2022-06-14 12:44:25 -0400

    implimented some of the api functions

--- src/main.go
+++ src/main.go
@@ -45,6 +45,70 @@ func GGTHXIdle(ggthx GGTHXSession, timeout int) GGTHXErrorCode {
 	return ggthx.DoPoll(timeout)
 }
 
+func GGTHXAddLocalInput(ggthx GGTHXSession, player GGTHXPlayerHandle, values []byte, size int) GGTHXErrorCode {
+	if ggthx == nil {
+		return GGTHX_ERRORCODE_INVALID_SESSION
+	}
+	return ggthx.AddLocalInput(&player, values, size)
+}
+
+func GGTHXSynchronizeInput(ggthx GGTHXSession, values []byte, size int, disconnectFlags *int) GGTHXErrorCode {
+	if ggthx == nil {
+		return GGTHX_ERRORCODE_INVALID_SESSION
+	}
+	return ggthx.SyncInput(values, size, disconnectFlags)
+}
+
+func GGTHXDisconnectPlayer(ggthx GGTHXSession, player GGTHXPlayerHandle) GGTHXErrorCode {
+	if ggthx == nil {
+		return GGTHX_ERRORCODE_INVALID_SESSION
+	}
+	return ggthx.DisconnectPlayer(&player)
+}
+
+func GGTHXAdvanceFrame(ggthx GGTHXSession) GGTHXErrorCode {
+	if ggthx == nil {
+		return GGTHX_ERRORCODE_INVALID_SESSION
+	}
+	return ggthx.IncrementFrame()
+}
+
+func GGTHXClientChat(ggthx GGTHXSession, text string) GGTHXErrorCode {
+	if ggthx == nil {
+		return GGTHX_ERRORCODE_INVALID_SESSION
+	}
+	return ggthx.Chat(text)
+}
+
+func GGTHXGetNetworkStats(ggthx GGTHXSession, player GGTHXPlayerHandle, stats *GGTHXNetworkStats) GGTHXErrorCode {
+	if ggthx == nil {
+		return GGTHX_ERRORCODE_INVALID_SESSION
+	}
+	return ggthx.GetNetworkStats(stats, player)
+}
+
+func GGTHXCloseSession(ggthx GGTHXSession) GGTHXErrorCode {
+	if ggthx == nil {
+		return GGTHX_ERRORCODE_INVALID_SESSION
+	}
+	ggthx.Close()
+	return GGTHX_OK
+}
+
+func GGTHXSetDisconnectTimeout(ggthx GGTHXSession, timeout int) GGTHXErrorCode {
+	if ggthx == nil {
+		return GGTHX_ERRORCODE_INVALID_SESSION
+	}
+	return ggthx.SetDisconnectTimeout(timeout)
+}
+
+func GGTHXSetDisconnectNotifyStart(ggthx GGTHXSession, timeout int) GGTHXErrorCode {
+	if ggthx == nil {
+		return GGTHX_ERRORCODE_INVALID_SESSION
+	}
+	return ggthx.SetDisconnectNotifyStart(timeout)
+}
+
 func main() {
 
 }
--- src/session.go
+++ src/session.go
@@ -6,12 +6,12 @@ package main
 type GGTHXSession interface {
 	DoPoll(timeout int) GGTHXErrorCode
 	AddPlayer(player *GGTHXPlayer, handle *GGTHXPlayerHandle) GGTHXErrorCode
-	AddLocalInput(player *GGTHXPlayer, values []byte, size int) GGTHXErrorCode
+	AddLocalInput(player *GGTHXPlayerHandle, values []byte, size int) GGTHXErrorCode
 	SyncInput(values []byte, size int, disconnect_flags *int) GGTHXErrorCode
 	IncrementFrame() GGTHXErrorCode
 	Chat(text string) GGTHXErrorCode
 	DisconnectPlayer(handle *GGTHXPlayerHandle) GGTHXErrorCode
-	GetNetworkStats(stats *GGTHXNetworkStats, handle *GGTHXPlayerHandle) GGTHXErrorCode
+	GetNetworkStats(stats *GGTHXNetworkStats, handle GGTHXPlayerHandle) GGTHXErrorCode
 	Logv(format string, args ...int) GGTHXErrorCode
 	SetFrameDelay(player GGTHXPlayerHandle, delay int) GGTHXErrorCode
 	SetDisconnectTimeout(timeout int) GGTHXErrorCode
